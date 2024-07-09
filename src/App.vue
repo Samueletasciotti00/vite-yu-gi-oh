878b2b7894ee92d529dd656b8ceedcdfcac93ef3
Samuele <samueletasciotti@icloud.com>
2024-07-09 18:55:08 +0200

    feat: layout titol

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,28 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader
+  }
+}
 </script>
 
 <template>
+  <!-- Titolo della Web App -->
+  <AppHeader message="Yu-gi-oh Api" />
+
+  <!-- Select -->
+
+  <!-- Container di cards -->
   
-  <HelloWorld />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss">
+
+  // General sass files
+  @use './style/general.scss';
+
+  
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<script>
+    export default {
+        name: 'App',
+        props: {
+            message: String
+        }
+    }
+</script>
+
+<template>
+
+    <!-- Bootstrap Titol of App -->
+   <div class="container-fluid text-secondary">
+        <div class="row-12">
+            <div class="col-12"><h1><img src="https://1000logos.net/wp-content/uploads/2024/02/Yu-Gi-Oh-Logo-2001.png" alt="">{{ message }}</h1></div>
+        </div>
+   </div>
+  
+</template>
+
+<style lang="scss" scoped>
+
+    // Immage logo settings
+    h1>img{
+        width: 100px;
+    }
+</style>
