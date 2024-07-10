0d34a15fd1f9bc41a4761e70540e8013bf8dc52c
Samuele <samueletasciotti@icloud.com>
2024-07-10 17:51:06 +0200

    feat: event change

--- src/components/AppSelect.vue
+++ src/components/AppSelect.vue
@@ -15,8 +15,8 @@ export default {
 
         <!-- Selct input per il filtro cards -->
         <select v-model="store.selectType[0]" name="ciao" id="">
-            <option  value="null" @option="$emit('search')">null</option>
-            <option  value="archetype">archetype</option>
+            <option  value="archetype" @change="$emit('search')">null</option>
+            <option  value="null">archetype</option>
             <option value="option2">Option 2</option>
             <option value="option3">Option 3</option>
  
